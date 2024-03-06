7cd8a6218b982c7e026b944fc6854ffad294138b
Michael Travers <michael.travers@udc.edu>
2024-03-05 22:35:29 -0500

    first push

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Hard Music In Progress</h1>
+      <ul>
+        <li>React</li>
+        <li>TypeScript</li>
+        <li>Node/ExpressJs</li>
+        <li>Oracle</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
